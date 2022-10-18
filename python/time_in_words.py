14fd678dd9301e2a54157607a6ed3619836690a8
tanmay002 <75234031+tanmay002@users.noreply.github.com>
2022-10-18 18:21:47 +0530

    updated

---
+++ python/time_in_words.py
@@ -0,0 +1,37 @@
+
+def timeInWords(h, m):
+
+    t=["zero", "one", "two", "three", "four", "five",
+       "six", "seven", "eight", "nine", "ten", "eleven",
+       "twelve", "thirteen", "fourteen", "fifteen",
+       "sixteen", "seventeen", "eighteen", "nineteen",
+       "twenty", "twenty one", "twenty two", "twenty three",
+       "twenty four", "twenty five" ,"twenty six",
+       "twenty seven", "twenty eight"," twenty nine"]
+
+    
+    
+    if(m==0):
+        print(t[h],"o' clock")
+    elif(m==15):
+        print("quarter past",t[h])
+    elif(m>=1 and m<=30 and m!=15):
+        print(t[m],"minutes past",t[h])
+    elif(m==45):
+        print("quarter to",t[(h%12)+1])
+    elif(m>30 and m<60 and m!=45):
+        print(t[60-m],"minutes to",t[(h%12)+1])
+
+
+hour=int(input("enter hours: "))
+minutes=int(input("enter minutes: "))
+
+timeInWords(hour,minutes)
+
+
+        
+
+   
+ 
+
+           
