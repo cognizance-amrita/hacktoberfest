4b94d97c6d6ba75eaaa25676103224ac5ffd71f5
Aman Chhetri <79894697+aman-chhetri@users.noreply.github.com>
2022-10-19 19:03:46 +0545

    Create Staircase.c

---
+++ C/Staircase.c
@@ -0,0 +1,24 @@
+// Staircase Problem
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n, i, j;
+    scanf("%d", &n);
+    
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            if (j < (n - 1 - i))
+                printf(" ");
+            else
+                printf("#");
+        }
+        printf("\n");
+    }
+    return 0;
+}
