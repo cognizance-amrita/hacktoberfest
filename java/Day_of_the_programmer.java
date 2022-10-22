16f391f19c19fedc9382d565b8efb8fb49335d79
Krithika S <90347901+kirthi0@users.noreply.github.com>
2022-10-22 12:15:49 +0530

    Day of the programmer in Java

---
+++ java/Day_of_the_programmer.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    
+	    Scanner sc = new Scanner(System.in);
+	    
+	    int y = sc.nextInt();
+	    
+	    if(y < 1918){
+	        if(y%4 == 0)
+	        System.out.println("12.09."+y);
+	        else
+	        System.out.println("13.09"+y);
+	    }
+	    
+	    else if(y > 1918){
+	        if(y%400 == 0 || (y%4 == 0 && y%100 != 0))
+	        System.out.println("12.09."+y);
+	        else
+	        System.out.println("13.09."+y);
+	    }
+	    
+	    else{
+	        System.out.println("26.09."+y);
+	    }
+		
+	}
+}
