b51a4e2dc95d1f49862aa0c51d674024e7ebafc4
Krithika S <90347901+kirthi0@users.noreply.github.com>
2022-10-22 16:21:07 +0530

    Updated Bill_division.java

--- java/Bill_division.java
+++ java/Bill_division.java
@@ -1,4 +1,5 @@
-
+import java.io.*;
+import java.util.*;
 import java.util.stream.*;
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
@@ -11,33 +12,30 @@ public static int bonAppetit(List<Integer> bill,int k,int b){
 	
 	c_amt = (sum - bill.get(k))/2;
 	
-	return b - c_amt;
-	
-
-	
-	
+	return (b - c_amt);	
 
 }
 public static void main(String[] args) throws IOException{
 	
 	BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 	
-	List<Integer> nk = Stream.of(bf.readLine().trim().replaceAll("\\s+$","").split(" ")
-	.map(Integer::parseInt)
-	.collect(toList());
+	List<Integer> nk = Stream.of(bf.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
 	
-	List<Integer>bill = stream.of(bf.readLine9).trim().replaceAll("\\s+$","").split(" ")
-	.map(Integer::parseInt)
-	.collect(toList());
+	List<Integer> bill = Stream.of(bf.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
 	
 	int b = Integer.parseInt(bf.readLine().trim());
 	int k = nk.get(1);
 	
-	System.out.println(bonAppetit(bill,k,b);
+	int ba = bonAppetit(bill,k,b);
 	
-	
-	
-
+	if(ba > 0)
+		System.out.println(ba);
+	else
+		System.out.println("Bon Appetit");
 
 }
 }
