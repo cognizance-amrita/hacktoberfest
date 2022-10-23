0362415f9cc153fd6b8f3d4cef421fa9b6c6f6a7
Aman Chhetri <79894697+aman-chhetri@users.noreply.github.com>
2022-10-24 02:02:01 +0545

    Compare The Triplets
    Issue: 66

---
+++ C/Compare_The_Triplets.c
@@ -0,0 +1,21 @@
+// Compare the Triplets
+
+#include <stdio.h>
+
+int main()
+{
+    int a[3],b[3];
+    scanf("%d %d %d", &a[0], &a[1], &a[2]);
+    scanf("%d %d %d", &b[0], &b[1], &b[2]);
+
+    int alice_score = 0,bob_score=0;
+    for (int i = 0; i < 3; i++)
+    {
+        if (a[i] > b[i])
+            alice_score++;
+        if (b[i] > a[i])
+            bob_score++;
+    }
+    printf("%d %d\n", alice_score, bob_score);
+    return 0;
+}
