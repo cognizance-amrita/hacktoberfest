dda6cbc3e3ddc6fb3d33a370a13d0f87c25a70c8
jaichiranjeeva <jaichiranjeevadadi@gmail.com>
2022-10-24 15:03:18 +0530

    Issue #70 : Resolve Bill divison issue in java & python
    resolves #70

---
+++ java/Bill_div.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Bill_div{
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n,k,sum=0,paid;
+        n=in.nextInt();
+        k=in.nextInt();
+        int[] orders = new int[n];
+        for(int i =0;i<n;i++){
+            orders[i]=in.nextInt();
+            sum+=orders[i];
+        }
+        paid=in.nextInt();
+        if(paid==((sum-orders[k])/2))
+            System.out.println("Bon Appetit");
+        else
+        System.out.println(paid-(sum-orders[k])/2);
+        
+    }
+}
